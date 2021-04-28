07fd90e5b04bd54e855ae87abe4555470bb10d2b
Anton <enitefall81@gmail.com>
2021-04-27 20:44:06 -0700

    All done

--- src/App.js
+++ src/App.js
@@ -21,15 +21,19 @@ function tell_BMI_color(bmi) {
 
 function Slider({minValue, maxValue, currentValue, onChange, title}) {
   return <div className="slidecontainer">
-    <input type="range" min={minValue} max={maxValue} value={currentValue} style={{width: "35rem"}}
-           onChange={e=> parseInt(onChange(e.target.value))} className="slider" id="myRange"/>
+    <label>
+     <h4>{title}</h4>
+      <input type="range" min={minValue} max={maxValue} value={currentValue} style={{width: "35rem"}}
+           onChange={e=> parseInt(onChange(e.target.value))} className="slider" id="myRange"
+    />
+    </label>
     {currentValue}
   </div>
 }
 
 function App() {
-let [weight, setWeight] = useState(0)
-let [height, setHeight] = useState(0)
+let [weight, setWeight] = useState(46)
+let [height, setHeight] = useState(175)
 
   return <div>
     <Slider minValue={140} maxValue={200}
@@ -40,6 +44,10 @@ let [height, setHeight] = useState(0)
     currentValue={weight} onChange={setWeight} title="Weight">
     Weight
     </Slider>
+    <div><h4>BMI {calculate_BMI(height, weight).toFixed(2)}</h4> </div>
+    <span className={`badge-${tell_BMI_color(calculate_BMI(height, weight))}`}>
+      {tell_BMI(calculate_BMI(height, weight))}
+    </span>
   </div>
 }
 
